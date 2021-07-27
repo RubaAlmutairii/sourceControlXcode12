f38503aadd1bf3269af924d637a3a6628bbd0762
Ruba <RubaAlmutairi@gmail.com>
2021-07-27 17:37:31 +0300

    Initial Commit

---
+++ sourceControlXcode12/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sourceControlXcode12
+//
+//  Created by Ruba Almutairi on 17/12/1442 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sourceControlXcode12/sourceControlXcode12App.swift
@@ -0,0 +1,17 @@
+//
+//  sourceControlXcode12App.swift
+//  sourceControlXcode12
+//
+//  Created by Ruba Almutairi on 17/12/1442 AH.
+//
+
+import SwiftUI
+
+@main
+struct sourceControlXcode12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
