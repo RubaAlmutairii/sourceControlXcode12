0799477d2698f2a8e75c0749b788cc4909023050
Ruba <RubaAlmutairi@gmail.com>
2021-07-28 10:07:17 +0300

    new branch added txt

--- sourceControlXcode12/ContentView.swift
+++ sourceControlXcode12/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world! xcode 12")
+        Text("Hello, world! xcode 12/ newBranch added ")
             .padding()
             .font(.title)
     }
